cb6c545ed470295e683413ea57dcdc41c6b68e75
Collin Gordon <colntrev@gmail.com>
2018-05-28 12:42:11 -0700

    Removed unused imports

--- src/main/java/BFS.java
+++ src/main/java/BFS.java
@@ -2,8 +2,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.util.LongAccumulator;
 import scala.Tuple2;
